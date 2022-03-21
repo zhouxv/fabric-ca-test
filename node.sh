15058f70f1cec49066be76923c508af9630c0dff
zhouxv <1598284508@qq.com>
2022-03-22 03:00:43 +0800

    改为localhost

--- node.sh
+++ node.sh
@@ -24,10 +24,10 @@ sudo cp $HOME/fabric-ca-test/hyperledger/org2/admin/msp/signcerts/cert.pem $HOME
 sudo cp $HOME/fabric-ca-test/hyperledger/org2/ca/crypto/ca-cert.pem $HOME/fabric-ca-test/nodeconfig/org0_order/organizations/org2/msp/cacerts
 sudo cp $HOME/fabric-ca-test/hyperledger/tls/ca/crypto/ca-cert.pem $HOME/fabric-ca-test/nodeconfig/org0_order/organizations/org2/msp/tlscacerts
 
+# 生成创世区块
 export FABRIC_CFG_PATH=$HOME/fabric-ca-test/nodeconfig/org0_order
-configtxgen -profile TwoOrgsOrdererGenesis -outputBlock /home/dinglab-dell/fabric-ca-test/nodeconfig/org0_order/genesis.block -channelID syschannel
-configtxgen -profile OrgsChannel -outputCreateChannelTx $HOME/fabric-ca-test/nodeconfig/org0_order/channel.tx -channelID mychannel
-
+configtxgen -profile TwoOrgsOrdererGenesis -outputBlock /home/dinglab-dell/fabric-ca-test/node/orderer-org0/genesis.block -channelID syschannel
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx /home/dinglab-dell/fabric-ca-test/nodeconfig/org0_order/channel.tx -channelID mychannel
 
 orderer
 
